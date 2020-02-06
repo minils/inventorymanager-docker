8edd928ba4e5339173f445aadc23d3ec7bc4e14d
Nils Schwabe <nils@schwabe.ws>
2020-02-06 17:24:59 +0100

    Update configuration. Works now

---
+++ python/init_and_run.sh
@@ -0,0 +1,6 @@
+cd /app
+./wait-for-it.sh -h db -p 3306 -t 0
+python3 manage.py migrate || { echo 'migrate failed' ; exit 1; }
+python3 manage.py collectstatic
+python3 manage.py loaddata inventory/fixtures/datasetup.json
+uwsgi --ini uwsgi-app.ini
